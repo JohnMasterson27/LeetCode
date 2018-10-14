a44a134b5ec42d2db20640831f6be932a732ee2e
John Masterson <JohnMasterson27@gmail.com>
2018-10-14 17:30:52 -0500

    Adding a bunch of solutions

---
+++ LeetCodeSolutions/Program.cs
@@ -0,0 +1,324 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LeetCodeSolutions
+{
+    public class LeetCodeSOlutions
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x)
+        {
+            val = x;
+        }
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+
+
+    public class ValidSudoku
+    {
+        public bool IsValidSudoku(char[,] board)
+        {
+            bool[] seenX;
+            int valX;
+            for (int i = 0; i < 9; i++)
+            {
+                seenX = new bool[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    valX = (int)board[i, j] - '1';
+                    if (valX >= 0 && valX <= 9)
+                    {
+                        if (seenX[valX]) { return false; }
+                        seenX[valX] = true;
+                    }
+                }
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                seenX = new bool[9];
+                for (int j = 0; j < 9; j++)
+                {
+                    valX = (int)board[j, i] - '1';
+                    if (valX >= 0 && valX <= 9)
+                    {
+                        if (seenX[valX]) { return false; }
+                        seenX[valX] = true;
+                    }
+                }
+            }
+
+            for (int offX = 0; offX < 3; offX++)
+            {
+                for (int offY = 0; offY < 3; offY++)
+                {
+                    seenX = new bool[9];
+                    for (int i = 0; i < 3; i++)
+                    {
+                        for (int j = 0; j < 3; j++)
+                        {
+                            valX = (int)board[offX * 3 + i, offY * 3 + j] - '1';
+                            if (valX >= 0 && valX <= 9)
+                            {
+                                if (seenX[valX]) { return false; }
+                                seenX[valX] = true;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            return true;
+        }
+    }
+
+    public class FirstMissingPositive
+    {
+
+        public int FirstMssing(int[] nums)
+        {
+            int val;
+
+            for(int i=0; i<nums.Length; i++)
+            {
+                val = nums[i];
+                if(val>0 && val<=nums.Length)
+                {
+                    nums[i] = nums[val - 1];
+                    nums[val - 1] = val;
+                }
+            }
+            for(int i=0; i<nums.Length; i++)
+            {
+                if(nums[i] != i+1)
+                {
+                    return i + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+
+    public class TrappingWater
+    {
+        public int Trap(int[] height)
+        {
+            int left, right, leftMax, rightMax, waterTrapped;
+
+            left = 0;
+            right = height.Length - 1;
+            leftMax = 0;
+            rightMax = 0;
+            waterTrapped = 0;
+
+            while (left < right)
+            {
+                if(height[left] <= height[right])
+                {
+                    leftMax = Math.Max(leftMax, height[left]);
+                    waterTrapped = waterTrapped + leftMax - height[left];
+                    left++;
+                }
+                else
+                {
+                    rightMax = Math.Max(rightMax, height[right]);
+                    waterTrapped = waterTrapped + rightMax - height[right];
+                    right--;
+                }
+            }
+
+            return waterTrapped;
+        }
+    }
+
+    public class ValidBinarySearchTree
+    {
+        public bool IsValidBST(TreeNode node)
+        {
+            int min, max;
+            return IsValidBSTHelper(node, out min, out max );
+        }
+
+        private bool IsValidBSTHelper(TreeNode node, out int min, out int max)
+        {
+            int minChild, maxChild;
+
+            min = node.val;
+            max = node.val;
+
+            if(node.left!=null) 
+            {
+                if(!IsValidBSTHelper(node.left, out minChild, out maxChild))
+                {
+                    return false;
+                }
+                if(maxChild >= node.val)
+                {
+                    return false;
+                }
+                min = minChild;
+            }
+
+            if(node.right != null)
+            {
+                if (!IsValidBSTHelper(node.right, out minChild, out maxChild))
+                {
+                    return false;
+                }
+                if (minChild <= node.val)
+                {
+                    return false;
+                }
+                max = maxChild;
+            }
+            return true;
+
+        }
+    }
+
+    public class RandomizedSet
+    {
+        int[] vals;
+        int nextIndex;
+        Dictionary<int, int> valMap;
+
+        public RandomizedSet()
+        {
+            valMap = new Dictionary<int, int>();
+            vals = new int[4];
+            nextIndex = 0;
+        }
+
+        public void Insert(int val)
+        {
+            int[] oldVals;
+            if (!valMap.ContainsKey(val))
+            {
+                valMap.Add(val, nextIndex);
+                if(nextIndex >= vals.Length)
+                {
+                    oldVals = vals;
+                    vals = new int[oldVals.Length * 2];
+                    for(int i=0; i<oldVals.Length; i++)
+                    {
+                        vals[i] = oldVals[i];
+                    }
+                }
+                vals[nextIndex] = val;
+                nextIndex++;
+            }
+        }
+
+        public void Remove(int val)
+        {
+            int index;
+            if(valMap.ContainsKey(val))
+            {
+                index = valMap[val];
+                valMap.Remove(val);
+                nextIndex--;
+                vals[index] = vals[nextIndex];
+            }
+        }
+
+        public int GetRandon()
+        {
+            int randomNum;
+            Random rand = new Random();
+
+            if(nextIndex<=0)
+            {
+                return -1;
+            }
+
+
+            randomNum = rand.Next(nextIndex-1);
+            return vals[randomNum];
+
+        }
+
+        public int[] GetListOfVals()
+        {
+            int minIndex,swap;
+            int[] ret;
+
+            ret = new int[nextIndex];
+            for(int i=0; i< nextIndex; i++)
+            {
+                ret[i] = vals[i];
+            }
+
+            for(int i=0; i< nextIndex; i++)
+            {
+                minIndex = i;
+                for(int j=i; j< nextIndex; j++)
+                {
+                    if(ret[j] < ret[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                swap = ret[i];
+                ret[i] = ret[minIndex];
+                ret[minIndex] = swap;
+            }
+            return ret;
+        }
+    }
+
+    public class AddTwoNums
+    {
+        public ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode retHead, nextNode;
+
+            int carry,val;
+
+            carry = 0;
+            retHead = new ListNode(0);
+            nextNode = retHead;
+
+            while (l2 != null || l1 != null || carry>0)
+            {
+                val = carry;
+                if(l2 != null)
+                {
+                    val = val + l2.val;
+                    l2 = l2.next;
+                } 
+
+                if(l1!= null)
+                {
+                    val = val + l1.val;
+                    l1 = l1.next;
+                }
+                carry = val / 10;
+                nextNode.next = new ListNode(val % 10);
+                nextNode = nextNode.next;
+            }
+            return retHead.next;
+        }
+    }
+}
+
+
---
+++ LeetCodeSolutionsTests/LeetCodeSolutionsTests.cs
@@ -0,0 +1,481 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCodeSolutions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeSolutions.Tests
+{
+    [TestClass()]
+    public class ValidSudokuTests
+    {
+        [TestMethod()]
+        public void TestValidSudoku()
+        {
+
+            ValidSudoku solution = new ValidSudoku();
+            bool output;
+
+            char[,] input =
+{
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+            };
+
+            output = solution.IsValidSudoku(input);
+
+            if (output == false)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestTwoInABox()
+        {
+
+            ValidSudoku solution = new ValidSudoku();
+            bool output;
+
+            char[,] input =
+{
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','6','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+            };
+
+            output = solution.IsValidSudoku(input);
+
+            if (output == true)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestTwoInAColumn()
+        {
+
+            ValidSudoku solution = new ValidSudoku();
+            bool output;
+
+            char[,] input =
+{
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','1'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+            };
+
+            output = solution.IsValidSudoku(input);
+
+            if (output == true)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestTwoInARow()
+        {
+
+            ValidSudoku solution = new ValidSudoku();
+            bool output;
+
+            char[,] input =
+{
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'2','6','.','.','.','.','2','8','3'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+            };
+
+            output = solution.IsValidSudoku(input);
+
+            if (output == true)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+
+    [TestClass()]
+    public class FirstMissingPositiveTests
+    {
+
+        [TestMethod()]
+        public void TestNoMissing()
+        {
+            int output;
+            FirstMissingPositive solution = new FirstMissingPositive();
+            int[] input = { 3, 1, 2, 5, 4, 6 };
+
+            output = solution.FirstMssing(input);
+
+            if (output != -1)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestSingleMissing()
+        {
+            int output;
+            FirstMissingPositive solution = new FirstMissingPositive();
+            int[] input = { 3, 1, -3, 5, 4, 6 };
+
+            output = solution.FirstMssing(input);
+
+            if (output != 2)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestMultipleMissing()
+        {
+            int output;
+            FirstMissingPositive solution = new FirstMissingPositive();
+            int[] input = { 3, 1, -3, 5, -4, 6 };
+
+            output = solution.FirstMssing(input);
+
+            if (output != 2)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestMissingWithDups()
+        {
+            int output;
+            FirstMissingPositive solution = new FirstMissingPositive();
+            int[] input = { 3, 1, 1, 5, 4, 6 };
+
+            output = solution.FirstMssing(input);
+
+            if (output != 2)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+
+    [TestClass()]
+    public class TrappingWaterTests
+    {
+
+        [TestMethod()]
+        public void TestCaseExample()
+        {
+            int output;
+            TrappingWater solution = new TrappingWater();
+            int[] input = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+            output = solution.Trap(input);
+
+            if (output != 6)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestNoHeights()
+        {
+            int output;
+            TrappingWater solution = new TrappingWater();
+            int[] input = { 0, 0, 0, 0 };
+
+            output = solution.Trap(input);
+
+            if (output != 0)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestOneWall()
+        {
+            int output;
+            TrappingWater solution = new TrappingWater();
+            int[] input = { 0, 0, 5, 0 };
+
+            output = solution.Trap(input);
+
+            if (output != 0)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestRightHighest()
+        {
+            int output;
+            TrappingWater solution = new TrappingWater();
+            int[] input = { 1, 0, 1, 0, 2, 0, 0, 1, 3 };
+
+            output = solution.Trap(input);
+
+            if (output != 7)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestMiddleHighest()
+        {
+            int output;
+            TrappingWater solution = new TrappingWater();
+            int[] input = { 1, 0, 1, 0, 2, 0, 0, 1, 3, 0, 3, 1, 2, 0 };
+
+            output = solution.Trap(input);
+
+            if (output != 11)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestLeftHighest()
+        {
+            int output;
+            TrappingWater solution = new TrappingWater();
+            int[] input = { 4, 1, 0, 1, 0, 2, 0, 0, 1, 3, 0, 3, 1, 2, 0 };
+
+            output = solution.Trap(input);
+
+            if (output != 23)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+
+    [TestClass()]
+    public class ValidBinarySearchTreeTests
+    {
+
+        [TestMethod()]
+        public void TestCaseExample()
+        {
+            bool output;
+            ValidBinarySearchTree solution = new ValidBinarySearchTree();
+            TreeNode input;
+
+            input = new TreeNode(2);
+            input.left = new TreeNode(1);
+            input.right = new TreeNode(3);
+
+            output = solution.IsValidBST(input);
+
+            if (output != true)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestCaseExample2()
+        {
+            bool output;
+            ValidBinarySearchTree solution = new ValidBinarySearchTree();
+            TreeNode input;
+
+            input = new TreeNode(5);
+            input.left = new TreeNode(1);
+            input.right = new TreeNode(4);
+            input.right.left = new TreeNode(3);
+            input.right.right = new TreeNode(6);
+
+            output = solution.IsValidBST(input);
+
+            if (output != false)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestCaseLargeTree()
+        {
+            bool output;
+            ValidBinarySearchTree solution = new ValidBinarySearchTree();
+            TreeNode input;
+
+            input = new TreeNode(5);
+            input.left = new TreeNode(1);
+            input.left.right = new TreeNode(4);
+            input.right = new TreeNode(8);
+            input.right.left = new TreeNode(6);
+            input.right.right = new TreeNode(10);
+
+            output = solution.IsValidBST(input);
+
+            if (output != true)
+            {
+                Assert.Fail();
+            }
+        }
+    }
+
+    [TestClass()]
+    public class RandomizedSetTests
+    {
+
+        [TestMethod()]
+        public void TestCaseInsertRandomRemove()
+        {
+            int random;
+            int[] vals;
+            RandomizedSet solution = new RandomizedSet();
+
+            solution.Insert(1);
+            vals = solution.GetListOfVals();
+            if (vals.Length != 1 || vals[0] != 1)
+            {
+                Assert.Fail("Insert Fail");
+            }
+
+            random = solution.GetRandon();
+            if (random != 1)
+            {
+                Assert.Fail("Random Fail");
+            }
+
+            solution.Remove(1);
+            vals = solution.GetListOfVals();
+            if (vals.Length != 0)
+            {
+                Assert.Fail("remove Fail");
+            }
+        }
+
+        [TestMethod()]
+        public void TestMultipleInsertAndRemove()
+        {
+            int[] vals;
+            RandomizedSet solution = new RandomizedSet();
+
+            solution.Insert(1);
+            solution.Insert(1);
+            solution.Insert(1);
+            solution.Insert(5);
+            solution.Insert(2);
+            solution.Insert(5);
+            vals = solution.GetListOfVals();
+            if (vals.Length != 3 || vals[0] != 1 || vals[1] != 2 || vals[2] != 5)
+            {
+                Assert.Fail("Insert Fail");
+            }
+
+            solution.Remove(2);
+            solution.Remove(2);
+            vals = solution.GetListOfVals();
+            if (vals.Length != 2 || vals[0] != 1 || vals[1] != 5)
+            {
+                Assert.Fail("remove Fail");
+            }
+        }
+    }
+
+    [TestClass()]
+    public class AddTwoNumsTests
+    {
+
+        [TestMethod()]
+        public void TestCaseNoLists()
+        {
+            ListNode ret;
+            AddTwoNums twoNums = new AddTwoNums();
+
+            ret = twoNums.AddTwoNumbers(null, null);
+
+            if (ret != null)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestListOne()
+        {
+            ListNode ret,l1,l2;
+            AddTwoNums twoNums = new AddTwoNums();
+            l1 = new ListNode(1);
+
+            ret = twoNums.AddTwoNumbers(l1, null);
+
+            if (ret == null || ret.val != 1 || ret.next != null)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestListTwo()
+        {
+            ListNode ret, l1, l2;
+            AddTwoNums twoNums = new AddTwoNums();
+            l2 = new ListNode(1);
+
+            ret = twoNums.AddTwoNumbers(null, l2);
+
+            if (ret == null || ret.val != 1 || ret.next != null)
+            {
+                Assert.Fail();
+            }
+        }
+
+        [TestMethod()]
+        public void TestMultipleWithCary()
+        {
+            ListNode ret, l1, l2;
+            AddTwoNums twoNums = new AddTwoNums();
+            l1 = new ListNode(8);
+            l1.next = new ListNode(6);
+            l2 = new ListNode(7);
+            l2.next = new ListNode(9);
+
+            ret = twoNums.AddTwoNumbers(l1, l2);
+
+
+            if ( ret.val != 5 || ret.next.val != 6 || ret.next.next.val != 1 || ret.next.next.next != null )
+            {
+                Assert.Fail();
+            }
+        }
+    }
+}
