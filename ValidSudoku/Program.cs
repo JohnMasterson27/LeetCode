f8f312853f48776e308593fe0c83ccba432cc6c2
John Masterson <JohnMasterson27@gmail.com>
2018-10-14 08:32:37 -0500

    Changing to .NET Framework, adding tests

--- ValidSudoku/Program.cs
+++ ValidSudoku/Program.cs
@@ -1,48 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 
-namespace LeetCodeSolutions
-
+namespace ValidSudoku
 {
-    class ValidSudoku
+    public class ValidSudoku
     {
-        static void BasicTest()
-        {
-            ValidSudoku solution = new ValidSudoku();
-            bool output;
-            char[,] input =
-            {
-                {'5','3','.','.','7','.','.','.','.'},
-                {'6','.','.','1','9','5','.','.','.'},
-                {'.','9','8','.','.','.','.','6','.'},
-                {'8','.','.','.','6','.','.','.','3'},
-                {'4','.','.','8','.','3','.','.','1'},
-                {'7','.','.','.','2','.','.','.','6'},
-                {'.','6','.','.','.','.','2','8','.'},
-                {'.','.','.','4','1','9','.','.','5'},
-                {'.','.','.','.','8','.','.','7','9'}
-            };
-
-            output = solution.IsValidSudoku(input);
-            Console.WriteLine(output);
-
-            Console.ReadLine();
-        }
-
-
         public bool IsValidSudoku(char[,] board)
         {
             bool[] seenX;
             int valX;
-            for(int i=0; i<9; i++)
+            for (int i = 0; i < 9; i++)
             {
                 seenX = new bool[9];
-                for(int j=0; j<9; j++)
+                for (int j = 0; j < 9; j++)
                 {
                     valX = (int)board[i, j] - '1';
-                    if(valX >= 0 && valX <=9)
+                    if (valX >= 0 && valX <= 9)
                     {
-                        if(seenX[valX]) { return false; }
+                        if (seenX[valX]) { return false; }
                         seenX[valX] = true;
                     }
                 }
@@ -62,16 +36,16 @@ namespace LeetCodeSolutions
                 }
             }
 
-            for (int offX=0; offX<3; offX++)
+            for (int offX = 0; offX < 3; offX++)
             {
-                for (int offY=0;  offY<3; offY++)
+                for (int offY = 0; offY < 3; offY++)
                 {
                     seenX = new bool[9];
                     for (int i = 0; i < 3; i++)
                     {
                         for (int j = 0; j < 3; j++)
                         {
-                            valX = (int)board[offX*3+i, offY*3+j] - '1';
+                            valX = (int)board[offX * 3 + i, offY * 3 + j] - '1';
                             if (valX >= 0 && valX <= 9)
                             {
                                 if (seenX[valX]) { return false; }
@@ -85,7 +59,14 @@ namespace LeetCodeSolutions
 
             return true;
         }
+
+        public static void Main(String[] args)
+        {
+            return;
+        }
     }
+
+
 }
 
 
